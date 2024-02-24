0eab4d238ba60990216e532db5f35aeddf71e3e9
RosanaMartinezA <rosamartinezaran@unal.edu.co>
2024-02-23 20:54:27 -0500

    intento 3

--- word_count.py
+++ word_count.py
@@ -1,5 +1,6 @@
+
 #
-# Escriba la función load_input que recive como parámetro un folder y retorna
+# Escriba la función load_input que recibe como parámetro un folder y retorna
 # una lista de tuplas donde el primer elemento de cada tupla es el nombre del
 # archivo y el segundo es una línea del archivo. La función convierte a tuplas
 # todas las lineas de cada uno de los archivos. La función es genérica y debe
@@ -13,9 +14,9 @@
 #     ('text2.txt'. 'hypotheses.')
 #   ]
 #
-
 import glob
 import fileinput
+import os.path
 
 def load_input(input_directory):
 
@@ -25,6 +26,7 @@ def load_input(input_directory):
         for line in f:
             sequence.append((fileinput.filename(), line))
     return sequence
+
 #
 # Escriba una función llamada maper que recibe una lista de tuplas de la
 # función anterior y retorna una lista de tuplas (clave, valor). En este caso,
@@ -43,12 +45,13 @@ def mapper(sequence):
     for _,text in sequence:
         words = text.split()
         for word in words:
-            word = word.replace(",", "")
-            word = word.replace(".", "")
+            word = word.replace(",","")
+            word = word.replace(".","")
             word = word.lower()
             new_sequence.append((word,1))
     return new_sequence
     
+
 
 #
 # Escriba la función shuffle_and_sort que recibe la lista de tuplas entregada
@@ -65,12 +68,14 @@ def shuffle_and_sort(sequence):
     sorted_sequence = sorted(sequence,key=lambda x: x[0])
     return sorted_sequence
 
+
+
 #
 # Escriba la función reducer, la cual recibe el resultado de shuffle_and_sort y
 # reduce los valores asociados a cada clave sumandolos. Como resultado, por
 # ejemplo, la reducción indica cuantas veces aparece la palabra analytics en el
 # texto.
-#
+
     
 def reducer(sequence):
 
@@ -100,12 +105,13 @@ def reducer(sequence):
     return new_sequence
 
 
+
 #
 # Escriba la función create_ouptput_directory que recibe un nombre de directorio
 # y lo crea. Si el directorio existe, la función falla.
 #
-import os.path
 
+import os.path
 def create_output_directory(output_directory):
     
     if os.path.exists(output_directory):
@@ -113,7 +119,6 @@ def create_output_directory(output_directory):
     os.makedirs(output_directory)
 
 
-#
 # Escriba la función save_output, la cual almacena en un archivo de texto llamado
 # part-00000 el resultado del reducer. El archivo debe ser guardado en el
 # directorio entregado como parámetro, y que se creo en el paso anterior.
@@ -131,27 +136,24 @@ def save_output(output_directory, sequence):
 # entregado como parámetro.
 #
 def create_marker(output_directory):
-   with open(output_directory + "/SUCCESS","w") as file:
-       file.write("")
-
+    with open(output_directory + "/_SUCCESS","w") as file:
+        file.write("")
 
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def job(input_directory, output_directory):
-    sequence = load_input(input_directory)
-    sequence = mapper(sequence)
-    sequence = shuffle_and_sort(sequence)
-    sequence = reducer(sequence) 
-    create_output_directory(output_directory)
-    save_output(output_directory, sequence)
-    create_marker(output_directory)
-    
+    sequence=load_input("input")
+    sequence=mapper(sequence)
+    sequence=shuffle_and_sort(sequence)
+    sequence=reducer(sequence) 
+    create_output_directory("output")
+    save_output("output", sequence)
+    create_marker("output")
 
 if __name__ == "__main__":
-    job(
-        "input",
-        "output",
-    )
-
+   job(
+       "input",
+       "output",
+   )
